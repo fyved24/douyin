e02f2339141ebabd1628966d9638567b1df900fb
fyved24 <fyved24@163.com>
2023-02-10 11:14:40 +0800

    fix: 上传视频截取第一帧当封面

--- handlers/video/publish_video_action.go
+++ handlers/video/publish_video_action.go
@@ -3,9 +3,11 @@ package video
 import (
 	"github.com/fyved24/douyin/models"
 	"github.com/fyved24/douyin/requests"
+	"github.com/fyved24/douyin/responses"
 	videoService "github.com/fyved24/douyin/services/video"
 	"github.com/fyved24/douyin/utils"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 	"path/filepath"
 )
@@ -17,19 +19,32 @@ var (
 
 func PublishVideoAction(c *gin.Context) {
 	req := requests.NewDouyinPublishActionRequest(c)
-
 	file, _ := c.FormFile("data")
 	ext := filepath.Ext(file.Filename)
-	filename := utils.NewFileName(req.UserID) + ext
-	videoFilePath := LocalStorage + filename
-	videoFileURL := FileServerPrefix + filename
+	name := utils.NewFileName(req.UserID)
+	videoFilename := name + ext
+	coverFilename := name + ".jpg"
+
+	videoFilePath := LocalStorage + videoFilename
+	videoFileURL := FileServerPrefix + videoFilename
+
+	coverFilePath := LocalStorage + coverFilename
+	coverFileURL := FileServerPrefix + coverFilename
 	err := c.SaveUploadedFile(file, videoFilePath)
+	err = utils.CutFirstFrameOfVideo(coverFilePath, videoFilePath)
+
 	if err != nil {
+		log.Printf("err %v", err)
+		c.JSON(http.StatusInternalServerError, responses.CommonResponse{
+			StatusCode: 1,
+			StatusMsg:  "获取封面失败",
+		})
 		return
 	}
 	video := &models.Video{
 		AuthorID: req.UserID,
 		PlayUrl:  videoFileURL,
+		CoverUrl: coverFileURL,
 		Title:    req.Title,
 	}
 	publishVideosRes, _ := videoService.SavePublishVideo(video)
