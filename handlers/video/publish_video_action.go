3cd58e6410c9e87072a014f86378b060a87ca638
fyved24 <fyved24@163.com>
2023-02-21 11:22:48 +0800

    fix: 使用协程上传视频

--- handlers/video/publish_video_action.go
+++ handlers/video/publish_video_action.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"github.com/fyved24/douyin/models"
 	"github.com/fyved24/douyin/requests"
-	"github.com/fyved24/douyin/responses"
 	"github.com/fyved24/douyin/services/comment"
 	videoService "github.com/fyved24/douyin/services/video"
 	"github.com/fyved24/douyin/utils"
@@ -24,40 +23,37 @@ var (
 func PublishVideoAction(c *gin.Context) {
 	req := requests.NewDouyinPublishActionRequest(c)
 	fileHeader, _ := c.FormFile("data")
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, _ := context.WithTimeout(context.Background(), 60*time.Second)
+	//copyContext := c.Copy()
+
 	file, _ := fileHeader.Open()
 	defer file.Close()
 	ext := filepath.Ext(fileHeader.Filename)
 	name := utils.NewFileName(req.UserID)
 	videoFilename := name + ext
 	coverFilename := name + ".jpeg"
-	object, err := models.MinIOClient.PutObject(ctx, videoBucket, videoFilename, file, fileHeader.Size, minio.PutObjectOptions{ContentType: "video/mp4"})
-	if err != nil {
-		return
-	}
-	log.Printf("视频上传成功 %v", object)
 
 	endpointURL := models.MinIOClient.EndpointURL().String()
 	videoFileURL := endpointURL + "/" + videoBucket + "/" + videoFilename
-
-	imageBuff := utils.CutFirstFrameOfVideo(videoFileURL)
-
-	object, err = models.MinIOClient.PutObject(ctx, imagesBucket, coverFilename, imageBuff, int64(imageBuff.Len()), minio.PutObjectOptions{ContentType: "image/jpeg"})
-	if err != nil {
-		return
-	}
-	log.Printf("视频上传成功 %v", object)
-
 	coverFileURL := endpointURL + "/" + imagesBucket + "/" + coverFilename
+	go func() {
+		log.Printf("视频上传")
 
-	if err != nil {
-		log.Printf("err %v", err)
-		c.JSON(http.StatusInternalServerError, responses.CommonResponse{
-			StatusCode: 1,
-			StatusMsg:  "获取封面失败",
-		})
-		return
-	}
+		object, err := models.MinIOClient.PutObject(ctx, videoBucket, videoFilename, file, fileHeader.Size, minio.PutObjectOptions{ContentType: "video/mp4"})
+		if err != nil {
+			return
+		}
+		log.Printf("视频上传成功 %v", object)
+
+		imageBuff := utils.CutFirstFrameOfVideo(videoFileURL)
+
+		object, err = models.MinIOClient.PutObject(ctx, imagesBucket, coverFilename, imageBuff, int64(imageBuff.Len()), minio.PutObjectOptions{ContentType: "image/jpeg"})
+		if err != nil {
+			return
+		}
+		log.Printf("封面上传成功 %v", object)
+	}()
+
 	video := &models.Video{
 		AuthorID: req.UserID,
 		PlayUrl:  videoFileURL,
@@ -68,4 +64,8 @@ func PublishVideoAction(c *gin.Context) {
 	c.JSON(http.StatusOK, publishVideosRes)
 	// 发布视频成功时更新本地缓存中用户信息的作品数
 	comment.ChangeUserCacheWorkCount(req.UserID)
+}
+
+func Upload() {
+
 }
